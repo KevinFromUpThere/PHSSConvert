fbecdd6a6c4db4a64e07fca57f698976b00de8e0
Kevin Archibald <karchibald@gmail.com>
2016-12-12 16:32:50 -0500

    updating powershell

--- Powershell/PHSSConvert.ps1
+++ Powershell/PHSSConvert.ps1
@@ -5,4 +5,29 @@ Param(
 )
 
 $source = Get-Content $Filepath
+$maxlines = $source.count
+write-host $maxlines total lines in the source file
 
+foreach ($line in $source)
+{
+
+#Regular Line of a song
+if $line.StartsWith("[a-z]")
+    {   
+        $CurrentSong = $CurrentSong + $line
+     }
+#check for new verse
+
+
+
+
+#Check for new song
+
+
+#Save file
+$OutPutFileName = $SongTitle + '.txt'
+$CurrentSong >> $OutPutFileName
+$CurrentSong = $null
+
+
+}    
