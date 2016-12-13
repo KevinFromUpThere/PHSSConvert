58b614382e33c628d064ab6da2a325e39f392b45
Kevin Archibald <karchibald@gmail.com>
2016-12-13 13:13:58 -0500

    adding function to check for new song, new verse,

--- Powershell/PHSSConvert.ps1
+++ Powershell/PHSSConvert.ps1
@@ -6,28 +6,34 @@ Param(
 
 $source = Get-Content $Filepath
 $maxlines = $source.count
+write-host Source File = $Filepath
 write-host $maxlines total lines in the source file
 
 foreach ($line in $source)
 {
 
-#Regular Line of a song
-if $line.StartsWith("[a-z]")
-    {   
-        $CurrentSong = $CurrentSong + $line
-     }
-#check for new verse
+    #Check for new song
+    #If new song, Save previous song to file
+     if ($line.StartsWith("[1-9]"))  #Add = but does not have a dot after the number
+        {     
+            $OutPutFileName = $line + '.txt'
+            $CurrentSong >> $OutPutFileName
+            $CurrentSong = $null
+        }
 
 
+    #check for new verse
+     if ($line.StartsWith("[1-9]"))  #Add - and DOES have a dot after the nunber
+        {     
 
+        }
 
-#Check for new song
+    #Regular Line of a song
+    if ($line.StartsWith("[a-z]"))
+        {   
+            $CurrentSong = $CurrentSong + $line
 
-
-#Save file
-$OutPutFileName = $SongTitle + '.txt'
-$CurrentSong >> $OutPutFileName
-$CurrentSong = $null
+        }
 
 
 }    
